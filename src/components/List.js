37df1888c94c197117d27d50ffc24c29e71884a9
JairoEBG <jairo.barquero>
2022-07-28 13:01:45 -0600

    React_JBG_C00960

---
+++ src/components/List.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const List = (props) => {
+    const { contents } = props;
+
+    if (!contents || contents.length === 0) return <h1>No hay animales</h1>
+
+
+    return (
+        <ul>
+            <h2 className="Component-Title">Animales</h2>
+            {contents.map((content) => {
+                return (
+                    <div style={{background: "yellow", margin: "10px"}}>
+                        <li key={content.id}>
+                            <span classname="Component-Title">{content.id}<b>Nombre</b> {content.name} <b> Especie {content.species}</b></span>
+                        </li>
+                    </div>
+                );
+            })}
+        </ul>
+    );
+
+};
+
+export default List;
